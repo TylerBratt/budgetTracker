dcdc3bd960dd3521216ef5ae63a1e1830625a6b5
Tyler Bratt <tyler~bratt@hotmail.com>
2022-01-12 13:55:03 -0500

    first commit

---
+++ src/components/BudgetCard.jsx
@@ -0,0 +1,59 @@
+import { Card, ProgressBar } from "react-bootstrap"
+import { Button, Stack } from "react-bootstrap"
+import { currencyFormatter } from "../utils"
+
+
+const BudgetCard = ({ name, amount, max, grey, onAddExpenseClick, onViewExpensesClick }) => {
+
+  const classNames = []
+  if (amount > max) {
+    classNames.push("bg-danger", "bg-opacity-10")
+  } else if (grey) {
+    classNames.push("bg-light")
+  }
+
+  const getProgressBarVarient = (amount, max) => {
+    const ratio = amount / max
+    if (ratio < .5) return "primary"
+    if (ratio < .75) return "warning"
+    return "danger"
+  }
+
+
+  return (
+    <Card className={classNames.join(" ")}>
+      <Card.Body>
+        <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
+          <div className="me-2">{name}</div>
+          <div className="d-flex align-items-baseline">
+            {currencyFormatter.format(amount)}
+            {max && (<span className="text-muted fs-6 ms-1"> / {currencyFormatter.format(max)}</span>)}
+            
+          </div>
+        </Card.Title>
+        <ProgressBar 
+          className="rounded-pill" 
+          variant={getProgressBarVarient(amount, max)}
+          min={0}
+          max={max}
+          now={amount}
+          />
+          <Stack direction="horizontal" gap="2" className="mt-4">
+            <Button 
+              variant="outline-primary" 
+              className="ms-auto"
+              onClick={onAddExpenseClick}
+            >
+              Add Expense
+            </Button>
+            <Button onClick={onViewExpensesClick} variant="outline-secondary">
+              View Expenses
+            </Button>
+
+          </Stack>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default BudgetCard
